f3db8c7210ff48f42a26ee561ed0eeaf77e4a69b
Sumit031202 <sumitkr.031202@gmail.com>
2026-01-15 02:48:51 +0530

    Refactor: use constants for log levels

--- internal/processing/process.go
+++ internal/processing/process.go
@@ -4,15 +4,20 @@ import (
 	"strings"
 )
 
+const (
+	ErrorKey="Error"
+	LevelCrit="Critical"
+	LevelInfo="Info"
+)
 
 func ProcessLog(line string) string {
 	return "Processed: " + line
 }
 
 func GetLogLevel(line string) string{
-	if(strings.Contains(line,"Error")){
-		return "Critical"
+	if(strings.Contains(line,ErrorKey)){
+		return LevelCrit
 	}else{
-		return "Info"
+		return LevelInfo
 	}
 }
