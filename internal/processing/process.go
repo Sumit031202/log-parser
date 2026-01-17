cd63cc0eb41055fe06b6358ef2259493c1061e51
Sumit031202 <sumitkr.031202@gmail.com>
2026-01-18 00:10:29 +0530

    feat: add LogEntry struct and parsing logic

--- internal/processing/process.go
+++ internal/processing/process.go
@@ -10,6 +10,31 @@ const (
 	LevelInfo="INFO"
 )
 
+type LogEntry struct{
+	Time string
+	Level string
+	Message string
+}
+
+func ParseLogLine(line string) LogEntry{
+	// divide the line into parts
+	// 2026-01-15 10:00:00 [INFO] System starting up
+	parts:=strings.Split(line, " ")
+
+	if len(parts)<4 {
+		return LogEntry{}
+	}
+	timeStamp:= parts[0] + " " + parts[1]
+	level:= strings.Trim(parts[2],"[]")
+	message:= strings.Join(parts[3:]," ")
+
+	return LogEntry{
+		Time: timeStamp,
+		Level: level,
+		Message: message,
+	}
+}
+
 func ProcessLog(line string) string {
 	return "Processed: " + line
 }
