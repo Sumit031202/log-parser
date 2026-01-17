fc4b5b67b561153f823afe0c41a8b386c4fa854b
Sumit031202 <sumitkr.031202@gmail.com>
2026-01-18 00:25:05 +0530

    chore: verify log parsing logic in main

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	// "github.com/Sumit031202/log-parser/internal/processing"
 	// "golang.org/x/text/message"
@@ -17,5 +18,14 @@ func main() {
 	fileName:=os.Args[1]
 	fmt.Printf("Reading the %s...\n",fileName)
 	fileContent:=reader.ReadFile(fileName)
+
+	lines:=strings.Split(fileContent,"\n")
+	// fmt.Println(lines)
+
+	for _, line := range lines {
+		// fmt.Println(line)
+		entry:=processing.ParseLogLine(line)
+		fmt.Printf("Time: %s, Level: %s, Message: %s\n",entry.Time,entry.Level,entry.Message)
+	}
 	fmt.Printf("Scan complete: caught %d critical errors",processing.CountCritical(fileContent))
 }
